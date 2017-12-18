7fcd9afc3d8816875a81e1e3ca2487b1a43601da
Casey Duckering <cduckering@gmail.com>
2017-12-18 15:32:54 -0800

    Add mirror Poincare Transform

--- poincare/Transform.py
+++ poincare/Transform.py
@@ -6,14 +6,18 @@ from .shapes import Point, Ideal
 
 
 class Transform:
-    def __init__(self, a,b,c,d,e=1):
+    def __init__(self, a,b,c,d,e=1,conj=False):
         self.abcd = a*e,b*e,c,d
+        self.conj = conj
     def __repr__(self):
-        return '{}({}, {}, {}, {})'.format(type(self).__name__,
+        return '{}({}, {}, {}, {}{})'.format(type(self).__name__,
                     *map(lambda z:complex(round(z.real,3),round(z.imag,3)),
-                         self.abcd))
+                         self.abcd),
+                    ', conj' if self.conj else '')
     def applyToTuple(self, point):
         z = complex(*point)
+        if self.conj:
+            z = z.conjugate()
         a,b,c,d = self.abcd
         zt = (a*z + b) / (c*z + d)
         return zt.real, zt.imag
@@ -40,17 +44,27 @@ class Transform:
         return self.applyToList(points, verify=verify)
     def inverted(self):
         a,b,c,d = self.abcd
-        return Transform(-d,b,c,-a)
+        if self.conj:
+            a,b,c,d = a.conjugate(), b.conjugate(), c.conjugate(), d.conjugate()
+        return Transform(-d,b,c,-a,conj=self.conj)
+    def conjugate(self):
+        a, b, c, d = self.abcd
+        return Transform(a.conjugate(), b.conjugate(),
+                         c.conjugate(), d.conjugate(), conj=not self.conj)
     @staticmethod
     def identity():
         return Transform(1,0,0,1,1)
     @staticmethod
     def merge(*transfoms):
         a,b,c,d = 1,0,0,1
-        for trans in transfoms:
+        conj = False
+        for i, trans in enumerate(transfoms):
             a2,b2,c2,d2 = trans.abcd
+            if trans.conj:
+                a,b,c,d = a.conjugate(), b.conjugate(), c.conjugate(), d.conjugate()
+                conj = not conj
             a,b,c,d = a*a2+c*b2, b*a2+d*b2, a*c2+c*d2, b*c2+d*d2
-        return Transform(a,b,c,d)
+        return Transform(a,b,c,d,conj=conj)
     @staticmethod
     def shiftOrigin(newOrigin, newX=None):
         z0 = complex(*newOrigin)
@@ -76,4 +90,10 @@ class Transform:
                 rad = math.radians(deg)
             e = cmath.rect(1, rad)
         return Transform(1,0,0,1,e)
+    @staticmethod
+    def mirror(p1=None, p2=(0,0)):
+        if p1 is None:
+            p1, p2 = p2, (1,0)
+        transShift = Transform.shiftOrigin(p1, p2)
+        return Transform.merge(transShift.conjugate(), transShift.inverted())
 
