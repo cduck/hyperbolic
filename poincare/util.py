42c601ad411d7ac21e028ac93e69968534758116
Casey Duckering <cduckering@gmail.com>
2017-12-18 15:31:28 -0800

    Add Poincare util function

--- poincare/util.py
+++ poincare/util.py
@@ -6,4 +6,6 @@ def radialEuclidToPoincare(r):
     return 2 * math.atanh(r)
 def radialPoincareToEuclid(r):
     return math.tanh(r/2)
+def poincareToEuclidFactor(hr):
+    return math.cosh(hr/2)**2 / 2
 
