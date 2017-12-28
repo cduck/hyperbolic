0548d5c4aa1e825bd84281e5a6ef6c98f6a87490
Casey Duckering <cduckering@gmail.com>
2017-12-27 23:12:36 -0800

    Add util function

--- hyperbolic/poincare/util.py
+++ hyperbolic/poincare/util.py
@@ -9,3 +9,12 @@ def radialPoincareToEuclid(r):
 def poincareToEuclidFactor(hr):
     return math.cosh(hr/2)**2 / 2
 
+def triangleSideForAngles(adj1, adj2, opposite):
+    ''' Uses the hyperbolic law of cosines, solved for the side length
+
+        Input angles in radians '''
+    A, B, C = adj1, adj2, opposite
+    c = math.acosh((math.cos(C) + math.cos(A)*math.cos(B)) /
+                   (math.sin(A)*math.sin(B)))
+    return c
+
